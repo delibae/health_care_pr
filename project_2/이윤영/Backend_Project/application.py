93c44172aa9b1da969b09af4d2b2eac71e395325
YoungLeigh <yoonyoung.lee1218@gmail.com>
2023-01-20 14:48:07 +0900

    .

--- project_2/이윤영/Backend_Project/application.py
+++ project_2/이윤영/Backend_Project/application.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import sys
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
   return render_template("index.html")
-
+@application.route("/applyhouse")
+def apply():
+  return render_template("applyhouse.html")
+@application.route("/applyphoto")
+def photo_apply():
+  location = request.args.get("location")
+  cleaness = request.args.get("clean")
+  built_in = request.args.get("built")
+  print(location, cleaness,built_in)
+  return render_template("applyhouse.html")
+@application.route("/findhouse")
+def list():
+  return render_template("findhouse.html")
   if __name__ == "__main__":
     application.run(host='0.0.0.0')
